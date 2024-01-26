5bdffa2deb116b60e8461ebfc1470a9e117927fc
muzihoon <youngsh92@gmail.com>
2024-01-26 17:51:32 +0900

    성공 확인

--- src/main/reactjs/src/App.js
+++ src/main/reactjs/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <img src={mainImg} style={{width:'650px'}} alt=""/>
       <h1><b>스프링부트 + 리액트 CI/CD 성공기원 !! 카리나의 응원 얍 </b></h1>
-        <h1>제발 나와라요</h1>
+        <h1>짝짝 성공입니다!</h1>
     </div>
   );
 }
