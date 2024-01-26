8270aade2f4042adb85a4e731be4b54b59ddf68e
muzihoon <youngsh92@gmail.com>
2024-01-26 17:47:18 +0900

    리액트 문구 변경

--- src/main/reactjs/src/App.js
+++ src/main/reactjs/src/App.js
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="App">
       <img src={mainImg} style={{width:'650px'}} alt=""/>
-      <h2><b>스프링부트 + 리액트 CI/CD 성공기원 !! 카리나의 응원 얍 </b></h2>
-        <b>테스트입니다.</b>
+      <h1><b>스프링부트 + 리액트 CI/CD 성공기원 !! 카리나의 응원 얍 </b></h1>
+        <h1>제발 나와라요</h1>
     </div>
   );
 }
