3e48aadf755b13d260e2ffd91c347327512beddb
muzihoon <youngsh92@gmail.com>
2024-02-04 07:02:35 +0900

    feat - AI Image 업로드 기능 추가
    - /api/ai_image/save 로 호출시 생성된 이미지 url로 접근, 파일 다운로드 후Object Storage에 업로드 진행
    
    - 임시로 다운로드 받은 파일 삭제 확인 기능 추가
    
    - properties 파일에 변경된 NCP 스토리지 접근키, 시크릿키 추가

--- src/main/java/com/example/demo/ai_image/controller/AI_ImageController.java
+++ src/main/java/com/example/demo/ai_image/controller/AI_ImageController.java
@@ -2,7 +2,10 @@ package com.example.demo.ai_image.controller;
 
 import com.example.demo.ai_image.dto.AI_ImageDto;
 import com.example.demo.ai_image.dto.AI_ImageRequestDto;
+import com.example.demo.ai_image.dto.ImageUploadRequestDto;
+import com.example.demo.ai_image.service.AI_FileService;
 import com.example.demo.ai_image.service.AI_ImageServiceImpl;
+import com.example.demo.ai_image.service.ImageUploader;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,11 +19,20 @@ import org.springframework.web.bind.annotation.*;
 public class AI_ImageController {
 
     private final AI_ImageServiceImpl aiImageService;
+    private final AI_FileService aiFileService;
+    private final ImageUploader imageUploader;
     //생성
     @PostMapping("/create")
     @ResponseStatus(HttpStatus.CREATED)
     public Object  createImage(final @Valid @RequestBody AI_ImageRequestDto imageDto) {
         return aiImageService.generateImage(imageDto);
+    }
+
+    //생성
+    @PostMapping("/save")
+    @ResponseStatus(HttpStatus.OK)
+    public void uploadImage(final @Valid @RequestBody ImageUploadRequestDto requestDto) {
+        imageUploader.uploadImageToBucket(requestDto);
     }
 
 }
