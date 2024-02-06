3d8ee2e81385ff91fedb67622a2b2bf19998307d
kimju123 <zkfltm7777@gmail.com>
2024-02-06 10:07:04 +0900

    feat : 시큐리티 세팅

--- src/main/java/com/example/demo/account/entity/Agreement.java
+++ src/main/java/com/example/demo/account/entity/Agreement.java
@@ -6,25 +6,31 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-//@Getter
-//@Entity
-//@Builder
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Table(name = "AGREEMENT")
+import java.sql.Timestamp;
+//############ 약관 동의 정보 ############
+@Getter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "AGREEMENT")
 public class Agreement {
-//
-//    @Id
-//    @Column(name = "ACCOUNT_ID", nullable = false)
-//    private Long accountId;
-//
-//    @Column(name = "AGREEMENT_USE_TERMS", nullable = false, columnDefinition = "BOOLEAN DEFAULT true")
-//    private boolean agreementUseTerms = true;
-//
-//    @Column(name = "AGREEMENT_INFO_OFFER", nullable = false)
-//    private boolean agreementInfoOffer = true;
-//
-//    @Column(name = "AGREEMENT_GET_NOTIFIED", nullable = false)
-//    private boolean agreementGetNotified;
+
+    @Id
+    @Column(name = "ACCOUNT_ID", nullable = false)
+    private Long accountId;
+
+    @Column(name = "AGREEMENT_USE_TERMS", nullable = false, columnDefinition = "BOOLEAN DEFAULT true")
+    private boolean agreementUseTerms = true;
+
+    @Column(name = "AGREEMENT_INFO_OFFER", nullable = false)
+    private boolean agreementInfoOffer = true;
+
+    @Column(name = "AGREEMENT_GET_NOTIFIED", nullable = false)
+    private boolean agreementGetNotified;
+
+    private Timestamp createdAt;
+
+    private Timestamp updatedAt;
 
 }
