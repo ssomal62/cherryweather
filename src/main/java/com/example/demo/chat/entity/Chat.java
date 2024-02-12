87e8aaf1e6f0b17d71114f00830fc4c25626ebf2
YOONHYUNHO <dbsgusgh2004@naver.com>
2024-02-12 12:59:22 +0900

    chore: 변경된 정적 파일 커밋

---
+++ src/main/java/com/example/demo/chat/entity/Chat.java
@@ -0,0 +1,33 @@
+//package com.example.demo.chat.entity;
+//
+//import jakarta.persistence.*;
+//import lombok.AccessLevel;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+//
+//@EntityListeners(AuditingEntityListener.class)
+//@Entity
+//@Getter
+//@Table(name = "chat")
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
+//public class Chat {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(updatable = false)
+//    private long chatId;
+//
+//    @Column(nullable = false, length = 20)
+//    private int chatNum;
+//
+//    @Column(nullable = false, length = 100)
+//    private String chatName;
+//
+//    @Column(nullable = false, length = 255)
+//    private String chatPhoto;
+
+
+
+
+//}
