6aa1ae72a09b8a110f960e250524bb6de3477dae
qkdrnfkzm <bbqcb@naver.com>
2024-02-13 16:43:01 +0900

    Feat: ip-api를 활용한 공인 ip 획득 기능 추가
    ip-api를 활용해 공인 ip 획득
    획득한 공인 ip를 GeoLocation에 사용

--- src/main/java/com/example/demo/weather/controller/GeoLocationController.java
+++ src/main/java/com/example/demo/weather/controller/GeoLocationController.java
@@ -3,11 +3,11 @@ package com.example.demo.weather.controller;
 import com.example.demo.weather.dto.GeoLocationReqDto;
 import com.example.demo.weather.dto.GeoLocationResDto;
 import com.example.demo.weather.service.GeoLocationService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,8 +18,8 @@ public class GeoLocationController {
     private final GeoLocationService geoLocationService;
 
     @GetMapping("/reqlocation")
-    public ResponseEntity<GeoLocationReqDto> getGeoLocation(@RequestParam String ip) {
-        GeoLocationReqDto location = geoLocationService.getGeoLocation(ip);
+    public ResponseEntity<GeoLocationReqDto> getGeoLocation() {
+        GeoLocationReqDto location = geoLocationService.getGeoLocation();
         if(location != null) {
             return ResponseEntity.ok(location);
         } else {
@@ -28,8 +28,8 @@ public class GeoLocationController {
     }
 
     @GetMapping("/reslocation")
-    public ResponseEntity<GeoLocationResDto> getGeoLocation2(@RequestParam String ip) {
-        GeoLocationResDto location = geoLocationService.convertLocation(ip);
+    public ResponseEntity<GeoLocationResDto> getGeoLocation2() {
+        GeoLocationResDto location = geoLocationService.convertLocation();
         if(location != null) {
             return ResponseEntity.ok(location);
         } else {
--- src/main/java/com/example/demo/weather/service/GeoLocationService.java
+++ src/main/java/com/example/demo/weather/service/GeoLocationService.java
@@ -3,10 +3,14 @@ package com.example.demo.weather.service;
 import com.example.demo.common.geoLocation.GeoLocationClient;
 import com.example.demo.weather.dto.GeoLocationReqDto;
 import com.example.demo.weather.dto.GeoLocationResDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @RequiredArgsConstructor
@@ -17,12 +21,16 @@ public class GeoLocationService {
 
     private final GeoConverter geoConverter;
 
-    public GeoLocationResDto convertLocation(String ip) {
-        GeoLocationReqDto reqDto = getGeoLocation(ip);
+    public GeoLocationResDto convertLocation() {
+        String ip = getClientPubliIP();
+        System.out.println("ip : " + ip);
+        GeoLocationReqDto reqDto = getGeoLocation();
         return geoConverter.toXY(reqDto);
     }
 
-    public GeoLocationReqDto getGeoLocation(String ip) {
+    public GeoLocationReqDto getGeoLocation() {
+        String ip = getClientPubliIP();
+        System.out.println("ip : " + ip);
         try {
             String response = geoLocationClient.run(ip);
             JsonNode root = objectMapper.readTree(response);
@@ -43,4 +51,31 @@ public class GeoLocationService {
             throw new RuntimeException(errorMessage);
         }
     }
+
+    // ip-api.com에서 공인 ip를 획득하고 반환합니다.
+    public String getClientPubliIP() {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "http://ip-api.com/json/";
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+
+        if(response.getStatusCode() == HttpStatus.OK) {
+            String responseBody = response.getBody();
+            // responseBody를 파싱하여 "query"의 값(IP 주소)를 추출합니다.
+            // Jackson ObjectMapper 또는 Gson을 사용하여 JSON을 파싱할 수 있습니다.
+            JsonNode jsonNode = null;
+            try {
+                jsonNode = objectMapper.readTree(responseBody);
+            } catch(JsonProcessingException e) {
+                e.getMessage();
+                // 예외 처리 필요
+                throw new RuntimeException(e);
+            }
+            return jsonNode.get("query").asText();
+        } else {
+            // 예외 처리 필요
+            return null;
+
+        }
+    }
+
 }
