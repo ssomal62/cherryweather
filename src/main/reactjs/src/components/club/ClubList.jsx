1fa38fc30a61c7fb5da2c40df732d180f99f43fe
ssomal62 <wxy890@gmail.com>
2024-02-14 16:56:22 +0900

    feat : club 페이지 추가

---
+++ src/main/reactjs/src/components/club/ClubList.jsx
@@ -0,0 +1,36 @@
+import React, {useEffect} from "react";
+import CardListItem from "./ClubListItem";
+import {useRecoilValue} from "recoil";
+import {useFetchClubs} from "../../recoil/hooks/UseFetchClubs";
+import {clubListState} from "../../recoil/hooks/UseFetchClubs";
+import styled from "styled-components"
+
+
+const ClubList = () => {
+
+    const fetchClubs = useFetchClubs();
+    const clubs = useRecoilValue(clubListState);
+
+    useEffect(() => {
+        fetchClubs();
+    }, [fetchClubs]);
+
+    return (
+        <div>
+            {clubs.map((club) => (
+                <CardListItemWrapper key={club.clubId}>
+                    <CardListItem
+                        club={club}
+                    />
+                </CardListItemWrapper>
+            ))}
+        </div>
+    )
+        ;
+}
+
+export default ClubList;
+
+const CardListItemWrapper = styled.div`
+  margin-bottom: 25px;
+`;
---
+++ src/main/reactjs/src/pages/club/Club.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../../common/Layout';
+import ClubList from "../../components/club/ClubList";
+
+const Club = () => {
+    return (
+        <Layout >
+            <ClubList/>
+        </Layout>
+    );
+};
+
+export default Club;
