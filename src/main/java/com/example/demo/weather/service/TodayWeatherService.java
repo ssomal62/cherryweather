47d26ebfa641917d73fe53ed9f850a39e34b7024
qkdrnfkzm <bbqcb@naver.com>
2024-02-14 16:54:26 +0900

    Feat: 오늘 날씨 조회 예외 처리 추가
    - 날씨 조회 기능에서 발생되는 코드에 따라 모든 예외 처리 기능 추가
    - 날짜 조회시 기준 되는 시간 현재 시각으로 변경
    - 서비스키 등 전역 변수 -> 지역 변수로 수정

--- src/main/java/com/example/demo/weather/service/TodayWeatherService.java
+++ src/main/java/com/example/demo/weather/service/TodayWeatherService.java
@@ -4,6 +4,7 @@ import com.example.demo.weather.dto.GeoLocationResDto;
 import com.example.demo.weather.dto.TodayWeatherReqDto;
 import com.example.demo.weather.dto.TodayWeatherResDto;
 import com.example.demo.weather.exception.LookupException;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
@@ -14,11 +15,12 @@ import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.example.demo.weather.exception.enums.WeatherExeptionStatus.WEATHER_INFO_LOOKUP_FAILED;
+import static com.example.demo.weather.exception.enums.WeatherExeptionStatus.*;
 
 
 @Service
@@ -27,12 +29,13 @@ public class TodayWeatherService {
 
     private final GeoLocationService geoLocationService;
 
-    private final String serviceKey = "mDNPPMp0rOT/VxVXKEJeQUDK1s169twmiSBFn4c/8pMRT/yFNC12SdZrV0hhVwDq7vn8b3D1fLOa3cmfWGv5hQ==";
-    private final String dataType = "JSON";
     private final String baseDate = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
-    private final String baseTime = "0600";
+    private final String baseTime = LocalTime.now().format(DateTimeFormatter.ofPattern("HH00"));
 
     public List<TodayWeatherReqDto> getTodayWeather() {
+
+        String serviceKey = "mDNPPMp0rOT/VxVXKEJeQUDK1s169twmiSBFn4c/8pMRT/yFNC12SdZrV0hhVwDq7vn8b3D1fLOa3cmfWGv5hQ==";
+        String dataType = "JSON";
 
         GeoLocationResDto geoLocationResDto = geoLocationService.convertLocation();
         int nx = (int) geoLocationResDto.getNx();
@@ -45,10 +48,51 @@ public class TodayWeatherService {
         System.out.println(url);
 
         // 예외처리
-        if(response.getStatusCode() != HttpStatus.OK || response.getBody().contains("errorCode")) {
-            throw new LookupException(WEATHER_INFO_LOOKUP_FAILED);
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode responseJson = null;
+        try {
+            responseJson = objectMapper.readTree(response.getBody());
+        } catch(JsonProcessingException e) {
+            throw new LookupException(JSON_PARSING_FAILED);
         }
-        return parseJsonResponse(response.getBody());
+        String resultCode = responseJson.path("response").path("header").path("resultCode").asText();
+        //resultCode에 따른 예외 처리
+        switch(resultCode) {
+            case "00": // 정상 처리
+                return parseJsonResponse(response.getBody());
+            case "01":
+                throw new LookupException(WEATHER_API_APPLICATION_ERROR);
+            case "02":
+                throw new LookupException(WEATHER_API_DB_ERROR);
+            case "03":
+                throw new LookupException(WEATHER_API_NODATA_ERROR);
+            case "04":
+                throw new LookupException(WEATHER_API_HTTP_ERROR);
+            case "05":
+                throw new LookupException(WEATHER_API_SERVICE_TIME_OUT);
+            case "10":
+                throw new LookupException(WEATHER_API_INVALID_REQUEST_PARAMETER_ERROR);
+            case "11":
+                throw new LookupException(WEATHER_API_NO_MANDATORY_REQUEST_PARAMETERS_ERROR);
+            case "12":
+                throw new LookupException(WEATHER_API_NO_OPENAPI_SERVICE_ERROR);
+            case "20":
+                throw new LookupException(WEATHER_API_SERVICE_ACCESS_DENIED_ERROR);
+            case "21":
+                throw new LookupException(WEATHER_API_TEMPORARILY_DISABLE_THE_SERVICEKEY_ERROR);
+            case "22":
+                throw new LookupException(WEATHER_API_LIMITED_NUMBER_OF_SERVICE_REQUESTS_EXCEEDS_ERROR);
+            case "30":
+                throw new LookupException(WEATHER_API_SERVICE_KEY_IS_NOT_REGISTERED_ERROR);
+            case "31":
+                throw new LookupException(WEATHER_API_DEADLINE_HAS_EXPIRED_ERROR);
+            case "32":
+                throw new LookupException(WEATHER_API_UNREGISTERED_IP_ERROR);
+            case "33":
+                throw new LookupException(WEATHER_API_UNSIGNED_CALL_ERROR);
+            default:
+                throw new LookupException(WEATHER_API_UNKNOWN_ERROR);
+        }
     }
 
     public List<TodayWeatherResDto> getFormattedTodayWeather() {
