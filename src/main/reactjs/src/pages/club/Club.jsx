65e03e13714277516b4f54bcd877e14dcbeb9ea1
ssomal62 <wxy890@gmail.com>
2024-02-16 17:11:21 +0900

    refactor: clubList 화면 수정
    - 분류 헤더 추가

--- src/main/reactjs/src/pages/club/Club.jsx
+++ src/main/reactjs/src/pages/club/Club.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Layout from '../../common/Layout';
-import ClubList from "../../components/club/ClubList";
+import ClubList from "../../components/club/clubList/ClubList";
+import ClubListHeader from "../../components/club/clubList/ClubListHeader";
 
 const Club = () => {
     return (
-        <Layout >
+        <Layout>
+            <ClubListHeader/>
+            <br/>
             <ClubList/>
         </Layout>
     );
