b66ecd02fd759ecce5805c4ed208d4599ce45e4c
lee-hyeyeon <lhy1415@knou.ac.kr>
2024-02-19 11:48:26 +0900

    Revert "feat - admin 채팅 기능 - 2"
    This reverts commit 957a092e63d9877ee2b44215fab470741c2d6d9e.

--- src/main/java/com/example/demo/ai_image/service/AI_ImageService.java
+++ src/main/java/com/example/demo/ai_image/service/AI_ImageService.java
@@ -1,6 +1,6 @@
-//package com.example.demo.ai_image.service;
-//
-//import com.example.demo.account.dto.AccountDetails;
-//import com.example.demo.account.entity.Account;
-//import com.example.demo.ai_image.dto.generate.generateRequestDto;
-//import com.example.demo.ai_image.dto.delete.Im
+package com.example.demo.ai_image.service;
+
+import com.example.demo.account.dto.AccountDetails;
+import com.example.demo.account.entity.Account;
+import com.example.demo.ai_image.dto.generate.generateRequestDto;
+import com.example.demo.ai_image.dto.delete.Im
