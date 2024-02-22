0c2daf92289c5be502c96cad9640930456178b77
qkdrnfkzm <bbqcb@naver.com>
2024-02-22 15:38:43 +0900

    Feat: 시간별 위성사진 조회 기능 추가

--- src/main/reactjs/src/pages/weather/WeatherDetail.jsx
+++ src/main/reactjs/src/pages/weather/WeatherDetail.jsx
@@ -2,12 +2,30 @@ import React from 'react';
 import Layout from "../../common/Layout";
 import HourlyWeather from "../../components/weather/HourlyWeather";
 import TodayWeather from "../../components/weather/TodayWeather";
+import SunMoonInfo from "../../components/weather/SunMoonInfo";
+import SatelliteImageViewer from "../../components/weather/SatelliteImageViewer";
 
 const WeatherDetail = () => {
     return (
         <Layout>
+            오늘 날씨
             <TodayWeather/>
+            시간별 날씨
             <HourlyWeather/>
+            일출 일몰
+            <SunMoonInfo/>
+            위성 화면
+            <SatelliteImageViewer/>
+            오늘 상세
+
+            주간 날씨
+
+
+            미세먼지
+
+            ...
+            ...
+
         </Layout>
     );
 };
