8d017619f283ebbd5635b18c71c65b0c9d6ea6f6
ssomal62 <wxy890@gmail.com>
2024-02-23 18:50:34 +0900

    refactor: club 기능 수정

--- src/main/java/com/example/demo/club/dto/ClubListDTO.java
+++ src/main/java/com/example/demo/club/dto/ClubListDTO.java
@@ -3,7 +3,6 @@ package com.example.demo.club.dto;
 
 import com.example.demo.club.domain.ClubSummary;
 
-import com.example.demo.club.entity.Club;
 import lombok.Builder;
 
 
@@ -13,28 +12,4 @@ import java.util.List;
 @Builder
 public record ClubListDTO(List<ClubSummary> summaryList) {
 
-    public static ClubListDTO fromClubs(List<Club> clubs) {
-        return ClubListDTO.builder()
-                .summaryList(
-                        clubs.stream()
-                                .map(ClubListDTO::convertToSummary)
-                                .toList()
-                )
-                .build();
-    }
-
-    private static ClubSummary convertToSummary(Club club) {
-        return ClubSummary.builder()
-                .clubId(club.getClubId())
-                .name(club.getName())
-                .code(club.getCode())
-                .description(club.getDescription())
-                .activitiesArea(club.getActivitiesArea())
-                .currentMembers(club.getCurrentMembers())
-                .maxMembers(club.getMaxMembers())
-                .status(club.getStatus())
-                .category(club.getCategory())
-                .grade(club.getGrade())
-                .build();
-    }
 }
