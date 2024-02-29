eb1c4b37e259afa12df0621e4944ca4348404f28
muzihoon <youngsh92@gmail.com>
2024-02-29 11:09:22 +0900

    refactor : GPT 디자인 변경 - 2차완료 / 빌드변경
    - 메인 페이지 변경
    - Header, Footer 변경
    - input 태그 변경

--- src/main/reactjs/src/components/ai/GPT/MainItem.jsx
+++ src/main/reactjs/src/components/ai/GPT/MainItem.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import {Card, Button, CardFooter, CardHeader, Image} from '@nextui-org/react';
 import { BiCloset } from "react-icons/bi";
-import './mainItem.css'; // Make sure to create this CSS file in your project
+import './mainItem.css';
+import {useNavigate} from "react-router-dom"; // Make sure to create this CSS file in your project
 const MainItem = () => {
+    const navigate = useNavigate();
     return (
         <>
-            <div className="title"> 체리의 소근소근 옷장 </div>
+            <div className="title"> 소곤소곤 옷장</div>
             <div className="max-w-[700px] w-full ">
                 <Card isFooterBlurred className="w-full h-[400px]">
-                    {/*<CardHeader className="absolute z-10 top-0 flex-col items-start bg-black bg-opacity-40">*/}
-                    {/*    <p className="text-tiny text-white uppercase font-bold">AI 오늘의 코디</p>*/}
-                    {/*    <h4 className="text-white font-medium text-xl">체리의 소곤소곤 옷장</h4>*/}
-                    {/*</CardHeader>*/}
                     <Image
                         removeWrapper
                         alt="Relaxing app background"
@@ -23,7 +21,8 @@ const MainItem = () => {
                             <BiCloset size="40px"/>
                             <div className="flex flex-col">
                                 <p className="text-medium uppercase font-bold text-black">체리의 소곤소곤 옷장</p>
-                                <p className="text-medium font-medium text-black">나만의 옷장에서 체리와 함께<br/> 오늘의 옷차림을 골라보세요
+                                <p className="text-medium font-medium text-black">전문 스타일리스트(?) 체리와 함께<br/> 오늘의 옷차림을
+                                    골라보세요
                                 </p>
                             </div>
                         </div>
@@ -31,13 +30,9 @@ const MainItem = () => {
                     </CardFooter>
                 </Card>
             </div>
-            <div className="title"> 체리의 추천 갤러리</div>
+            <div className="title"> 비밀 옷장</div>
             <div className="max-w-[700px] w-full ">
                 <Card isFooterBlurred className="w-full h-[400px]">
-                    <CardHeader className="absolute z-10 top-0 flex-col items-start bg-black bg-opacity-40">
-                        <p className="text-tiny text-white uppercase font-bold">AI 오늘의 코디</p>
-                        <h4 className="text-white font-medium text-xl">체리의 소곤소곤 옷장</h4>
-                    </CardHeader>
                     <Image
                         removeWrapper
                         alt="Relaxing app background"
@@ -48,8 +43,8 @@ const MainItem = () => {
                         <div className="flex flex-grow gap-2 items-center">
                             <BiCloset size="40px"/>
                             <div className="flex flex-col">
-                                <p className="text-medium uppercase font-bold text-black">체리의 소곤소곤 옷장</p>
-                                <p className="text-medium font-medium text-black">나만의 옷장에서 체리와 함께<br/> 오늘의 옷차림을 골라보세요
+                                <p className="text-medium uppercase font-bold text-black">체리의 비밀 옷장</p>
+                                <p className="text-medium font-medium text-black">내가 선택한 옷을 보관하는<br/> 비밀 옷장이에요!
                                 </p>
                             </div>
                         </div>
@@ -57,13 +52,31 @@ const MainItem = () => {
                     </CardFooter>
                 </Card>
             </div>
-            <div className="title"> 랜덤 옷 추천</div>
+            <div className="title"> 야심찬 전시실</div>
             <div className="max-w-[700px] w-full ">
                 <Card isFooterBlurred className="w-full h-[400px]">
-                    <CardHeader className="absolute z-10 top-0 flex-col items-start bg-black bg-opacity-40">
-                        <p className="text-tiny text-white uppercase font-bold">AI 오늘의 코디</p>
-                        <h4 className="text-white font-medium text-xl">체리의 소곤소곤 옷장</h4>
-                    </CardHeader>
+                    <Image
+                        removeWrapper
+                        alt="Relaxing app background"
+                        className="z-0 w-full h-full object-cover"
+                        src="https://kr.object.ncloudstorage.com/cherry-ai-image/youngsh92%40naver.com/54619020071927277513d1fcd13-99fb-41e6-ac4f-7e3477010d92.png"
+                    />
+                    <CardFooter className="absolute bg-white/80 bottom-0 z-10  border-default-600 ">
+                        <div className="flex flex-grow gap-2 items-center">
+                            <BiCloset size="40px"/>
+                            <div className="flex flex-col">
+                                <p className="text-medium uppercase font-bold text-black">체리의 야심찬 전시실</p>
+                                <p className="text-medium font-medium text-black">체리가 자신있게 추천하는 <br/> Made in Cherry의 여러 스타일..!
+                                </p>
+                            </div>
+                        </div>
+                        <Button radius="full" size="sm" onClick={() => navigate('/imageList')}>이동하기</Button>
+                    </CardFooter>
+                </Card>
+            </div>
+            <div className="title"> 꿈꾸는 옷장</div>
+            <div className="max-w-[700px] w-full ">
+                <Card isFooterBlurred className="w-full h-[400px]">
                     <Image
                         removeWrapper
                         alt="Relaxing app background"
@@ -74,8 +87,9 @@ const MainItem = () => {
                         <div className="flex flex-grow gap-2 items-center">
                             <BiCloset size="40px"/>
                             <div className="flex flex-col">
-                                <p className="text-medium uppercase font-bold text-black">체리의 소곤소곤 옷장</p>
-                                <p className="text-medium font-medium text-black">나만의 옷장에서 체리와 함께<br/> 오늘의 옷차림을 골라보세요
+                                <p className="text-medium uppercase font-bold text-black">체리의 꿈꾸는 옷장</p>
+                                <p className="text-medium font-medium text-black">랜덤으로 생성되는 옷차림이에요.<br/> 어떤옷이 나올지 체리도
+                                    몰라요!
                                 </p>
                             </div>
                         </div>
