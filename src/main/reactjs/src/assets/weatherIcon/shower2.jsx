725dd7e9335232d4da519cb025eaf2a8d97d04d5
qkdrnfkzm <bbqcb@naver.com>
2024-03-05 15:03:18 +0900

    Design: 메인 페이지 디자인
    - 메인 페이지 아이콘 디자인 수정

--- src/main/reactjs/src/assets/weatherIcon/shower2.jsx
+++ src/main/reactjs/src/assets/weatherIcon/shower2.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-const SvgSunShower = ({fill = '#7383A1', stroke='#7383A1', width='13em', height='13em', ...props}) => (
+const SvgSunShower = ({fill = '#FFFFFF', stroke='#FFFFFF', width='2.3em', height='13em', ...props}) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 375.76 418.03"
